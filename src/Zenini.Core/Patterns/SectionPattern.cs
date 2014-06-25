cce448b1e140d5ad326c63b4a19dd0d4bd5d84da
Tom Marien <tommarien@gmail.com>
2014-06-25 21:14:15 +0200

    Start pattern recognition of Sections

---
+++ src/Zenini.Core/Patterns/SectionPattern.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Zenini.Core.Patterns
+{
+    public class SectionPattern
+    {
+        private readonly Regex _regex = new Regex(@"\A\[(.*)\]\z", RegexOptions.Compiled);
+
+        public bool Matches(string value)
+        {
+            return _regex.IsMatch(value);
+        }
+    }
+}
---
+++ src/Zenini.Tests/Patterns/Section/When_matching_a_string_for_a_section.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Zenini.Core.Patterns.Section
+{
+    [TestFixture]
+    public class When_matching_a_string_for_a_section
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Pattern = new SectionPattern();
+        }
+
+        public SectionPattern Pattern { get; set; }
+
+        public IEnumerable<string> TestValues
+        {
+            get
+            {
+                yield return "[SectionA]";
+                yield return "[A Section]";
+                yield return "[ A Section]";
+                yield return "[A Section ]";
+            }
+        }
+
+        [Test]
+        [TestCaseSource("TestValues")]
+        public void it_matches_string(string line)
+        {
+            Pattern.Matches(line).ShouldBe(true);
+        }
+    }
+}
