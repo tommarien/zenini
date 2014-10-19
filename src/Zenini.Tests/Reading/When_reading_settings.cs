fe3cbd46eefa5ddc1efa347ef4e6e3e25c499c2f
Tom Marien <tommarien@gmail.com>
2014-10-19 14:18:22 +0200

    Support comments on values

--- src/Zenini.Tests/Reading/When_reading_settings.cs
+++ src/Zenini.Tests/Reading/When_reading_settings.cs
@@ -53,6 +53,16 @@ namespace Zenini.Tests.Reading
         }
 
         [Test]
+        public void it_ignores_trailing_whitespace_on_value_with_comments()
+        {
+            Source.AppendLine("test=value  ;comment");
+
+            IIniSettings settings = ReadFromSource();
+
+            settings["Section"].GetValue("test").ShouldBe("value");
+        }
+
+        [Test]
         public void it_ignores_trailing_whitespaces_on_key()
         {
             Source.AppendLine("test   =value");
@@ -63,9 +73,29 @@ namespace Zenini.Tests.Reading
         }
 
         [Test]
+        public void it_ignores_trailing_whitespaces_on_key_with_comments()
+        {
+            Source.AppendLine("test   =value;comment");
+
+            IIniSettings settings = ReadFromSource();
+
+            settings["Section"].GetValue("test").ShouldBe("value");
+        }
+
+        [Test]
         public void it_reads_the_setting()
         {
             Source.AppendLine("test=value");
+
+            IIniSettings settings = ReadFromSource();
+
+            settings["Section"].GetValue("test").ShouldBe("value");
+        }
+
+        [Test]
+        public void it_reads_the_setting_ignore_comments()
+        {
+            Source.AppendLine("test=value;comment");
 
             IIniSettings settings = ReadFromSource();
 
--- src/Zenini/Patterns/KeyValuePattern.cs
+++ src/Zenini/Patterns/KeyValuePattern.cs
@@ -5,7 +5,7 @@ namespace Zenini.Patterns
 {
     public class KeyValuePattern
     {
-        private static readonly Regex KeyValueRegex = new Regex(@"^\s*(.+?)\s*=\s*(?:"")?(.+?)(?:\"")?\s*$", RegexOptions.Compiled);
+        private static readonly Regex KeyValueRegex = new Regex(@"^\s*(.+?)\s*=\s*?([^;]*)?\s*;?.*$", RegexOptions.Compiled);
 
         public virtual bool Matches(string line)
         {
@@ -18,6 +18,13 @@ namespace Zenini.Patterns
 
             string key = collection[0].Groups[1].Value;
             string value = collection[0].Groups[2].Value;
+
+            if (value.StartsWith("\"") && value.EndsWith("\""))
+                value = value.Substring(1, value.Length - 2);
+            else
+            {
+                value = value.Trim();
+            }
 
             return new Tuple<string, string>(key, value);
         }
